8c941a9a5036296cb012cc1f9ef163aa9cc3fcae
sbhrepo <sbhrepo@users.noreply.github.com>
2018-10-04 22:11:19 -0400

    Challenge 9&10  week 5

--- js/script.js
+++ js/script.js
@@ -24,11 +24,16 @@ function switchChannel(channelObject) {
     //Log the channel switch
     console.log("Tuning in to channel", channelObject);
 
+    // call abort() only in case of clicking on change channel during create new channel
+    if (editingMode == true) {
+        abort();
+    }
+
     // #7  Write the new channel to the right app bar using object property
     document.getElementById('channel-name').innerHTML = channelObject.name;
 
     //#7  change the channel location using object property
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/'
+    document.getElementById('channel-location').innerHTML = ' by <a href="http://w3w.co/'
         + channelObject.createdBy
         + '" target="_blank"><strong>'
         + channelObject.createdBy
@@ -78,8 +83,13 @@ function selectTab(tabId) {
 /**
  * toggle (show/hide) the emojis menu
  */
+var emojis = require('emojis-list');
+console.log(emojis[0]);
 function toggleEmojis() {
     $('#emojis').toggle(); // #toggle
+    if ($('#emojis').is(":visible")) {
+        document.getElementById('emojis').innerHTML = emojis;
+    }
 }
 
 /**
@@ -93,7 +103,7 @@ function Message(text) {
     this.latitude = currentLocation.latitude;
     this.longitude = currentLocation.longitude;
     // set dates
-    this.createdOn = new Date() //now
+    this.createdOn = new Date(); //now
     this.expiresOn = new Date(Date.now() + 15 * 60 * 1000); // mins * secs * msecs
     // set text
     this.text = text;
@@ -107,7 +117,15 @@ function sendMessage() {
 
     // #8 let's now use the real message #input
     var message = new Message($('#message').val());
+
+    if (message.text.length == 0) {
+        console.log("New message is empty." + message.text.length);
+        return;
+    }
     console.log("New message:", message);
+
+    currentChannel.messages.push(message.text);
+    currentChannel.messageCount += 1;
 
     // #8 convenient message append with jQuery:
     $('#messages').append(createMessageElement(message));
@@ -140,21 +158,37 @@ function createMessageElement(messageObject) {
         messageObject.createdOn.toLocaleString() +
         '<em>' + expiresIn+ ' min. left</em></h3>' +
         '<p>' + messageObject.text + '</p>' +
-        '<button>+5 min.</button>' +
+        '<button class="accent">+5 min.</button>' +
         '</div>';
 }
 
 
-function listChannels() {
+function listChannels(criterion) {
     // #8 channel onload
     //$('#channels ul').append("<li>New Channel</li>")
 
     // #8 five new channels
-    $('#channels ul').append(createChannelElement(yummy));
-    $('#channels ul').append(createChannelElement(sevencontinents));
-    $('#channels ul').append(createChannelElement(killerapp));
-    $('#channels ul').append(createChannelElement(firstpersononmars));
-    $('#channels ul').append(createChannelElement(octoberfest));
+    //$('#channels ul').append(createChannelElement(yummy));
+    //$('#channels ul').append(createChannelElement(sevencontinents));
+    //$('#channels ul').append(createChannelElement(killerapp));
+    //$('#channels ul').append(createChannelElement(firstpersononmars));
+    //$('#channels ul').append(createChannelElement(octoberfest));
+
+    //Channels are appended to the channels list with one loop
+    if (criterion == "new") {
+        channels.sort(compareNew);
+    } else if (criterion == "trending") {
+        channels.sort(compareTrending);
+    } else if (criterion == "favorites") {
+        channels.sort(compareFavorites);
+    } else {
+        console.log('You fucked it up, the criterion is:', criterion);
+    }
+    $('#channels ul').empty();
+    var i;
+    for (i = 0; i < channels.length; i++) {
+        $('#channels ul').append(createChannelElement(channels[i]));
+    }
 }
 
 /**
@@ -192,4 +226,110 @@ function createChannelElement(channelObject) {
 
     // return the complete channel
     return channel;
+}
+
+// compare base on creation time
+function compareNew (channel1, channel2) {
+    if (channel1.createdOn < channel2.createdOn) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+// compare base on number of messages
+function compareTrending (channel1, channel2) {
+    if (channel1.messageCount < channel2.messageCount) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+// compare base on starred mode
+function compareFavorites (channel1, channel2) {
+    if (channel1.starred < channel2.starred) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+// change the interface to editing mode for adding new channel
+var editingMode = false;
+function createNewChannel () {
+    console.log("createNewChannel");
+    editingMode = true;
+    document.getElementById('rightAppBar').innerHTML = '<input type="text" placeholder="Enter a #ChannelName" maxlength="60" id="newChannelName"><button class="primary-blue" id="abort" onclick="abort()">X ABORT</button>';
+    $('button.sendButton').replaceWith('<button class="accent sendButton" onclick="createChannel()">CREATE</button>');
+    //$('#chat h1').html = '<p>hello world!!!</p>';
+    //<input type="submit" class="primary-blue" id="abort" value="X ABORT">
+}
+
+// create new channel in case of valid fields
+function createChannel() {
+    console.log("createChannel");
+
+    // check if the message field is empty
+    var theMessage = new Message($('#message').val());
+    var theChannelName = document.getElementById("newChannelName").value;
+    console.log("message="+theMessage.text);
+    console.log("theChannelName="+theChannelName);
+
+    // check if any of the fields are empty
+    if (theMessage.text.length == 0 || theChannelName.length == 0) {
+        console.log("New message ("+theMessage.text.length+") or channel name ("+theChannelName.length+")is empty.");
+        abort();
+        return;
+    }
+    // check for spaces in the channel name
+    if (theChannelName.indexOf(' ') !== -1) {
+        console.log("channel name contain spaces ["+theChannelName+"]");
+        abort();
+        return;
+    }
+    // check if channel name start with #
+    if (theChannelName.charAt(0) != '#' ) {
+        console.log("channel name not start with # ["+theChannelName+"]");
+        abort();
+        return;
+    }
+
+    // creat new channel and add it to the channels array
+    var newChaneel = new Channel(theChannelName);
+    newChaneel.message.push(theMessage.text);
+    channels.push(newChaneel);
+
+    // set the new channel as the current channel
+    switchChannel(newChaneel);
+    //sendMessage();
+    // clear the message input
+    $('#message').val('');
+}
+
+// exit from editing mode - create new channel 
+function abort() {
+    console.log("abort");
+    editingMode = false;
+    document.getElementById('rightAppBar').innerHTML = '<span id="channel-name">' + currentChannel.name + '</span><small id="channel-location"> by <strong>' + currentChannel.createdBy + '</strong></small><i class="' + (currentChannel.starred ?'fas':'far') + ' fa-star" onclick="star()"></i>';
+    $('button.sendButton').replaceWith('<button class="accent sendButton" onclick="sendMessage()"><i class="fas fa-arrow-right"></i></button>');
+    // clear the message input
+    $('#message').val('');
+
+    /*switchChannel(currentChannel);      this line will bring the channle back to life but it not needed, when ever they will fix the issue of the channels it will be fixed*/
+}
+
+/**
+ * This #constructor function creates a new chat channel.
+ * @param text `String` a chaneel name
+ * @constructor
+ */
+function Channel(text) {
+    // channel name
+    this.name = text;
+    // set dates
+    this.createdOn = new Date(); //now
+    this.createdBy = "cheeses.yard.applies";
+    this.starred = true;
+    this.expiresIn = 60;
+    this.messageCount = 0;
+    this.message = [];
 }
